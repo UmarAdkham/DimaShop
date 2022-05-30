597af042c9e41ed5ef5cb754b71cc4017430552a
abdulloh889 <99891087+abdulloh889@users.noreply.github.com>
2022-05-30 11:20:58 +0500

    Kerakmas fayllarni o'chriib tashladim

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Admin from "./Admin";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Admin/>);
+root.render(<h1>Salom</h1>);
