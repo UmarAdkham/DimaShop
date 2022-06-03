d7eb53b6e4f899816f81d3a37855c2e65c78e119
BekhruzValijonov <103342098+BekhruzValizhonov@users.noreply.github.com>
2022-06-03 10:57:03 +0500

    h

--- src/index.js
+++ src/index.js
@@ -2,28 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Login from "./components/Login";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddShop from "./components/AddShop.jsx";
 import LogUp from "./components/LogUp.jsx";
-import AddAdmin from "./components/AddAdmin.jsx";
+// import AddAdmin from "./components/AddAdmin.jsx";
 import Layout from "./components/Layout.jsx";
 
-
 function Cibo() {
-    return (
-        <BrowserRouter>
-            <Routes>
-            <Route path="/" element={<Layout/>}>
-          <Route path="AddAdmin" element={<AddAdmin />} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index path="Login" element={<Login />} />
+          {/* <Route path="AddAdmin" element={<AddAdmin />} /> */}
           <Route path="AddShop" element={<AddShop />} />
           <Route path="LogUp" element={<LogUp />} />
-          <Route path="Login" element={<Login />} />
         </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Cibo />);
